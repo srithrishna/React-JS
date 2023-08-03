01bd1e848613b6b67a766462ff2e48097ce1b83c
srithrishna <thrishna23jayakumar@gmail.com>
2023-08-03 11:46:47 +0530

    2

---
+++ jss/sample.js
@@ -0,0 +1,22 @@
+var a = 120
+/* console.log("This is the value before :" + a)
+{
+    var a =20
+    console.log("This is after changing" + a)
+}
+console.log("This is the value before :" + a)
+ */
+let b = 140
+console.log("This is the value:" + b)
+{
+    let b =20
+    console.log("This is after changing " + b)  
+}
+console.log("This is the value:" + b)
+
+const c = 140
+console.log("This is the value:" + c)
+
+const d= 5
+console.log("This is the value:" + d)
+
